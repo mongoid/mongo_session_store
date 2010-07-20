296df6c48b2b53b127fd00180d257e4cd5cad297
Ryan Fitzgerald <ryan@gymlogr.com>
2010-07-20 03:20:31 -0400

    switched session ids to being rails generated ids, rather than just mongo objectids, which are easy to guess

--- lib/mongo_session_store/mongoid.rb
+++ lib/mongo_session_store/mongoid.rb
@@ -9,7 +9,10 @@ module ActionDispatch
         include Mongoid::Timestamps
 
         field :data, :type => String, :default => [Marshal.dump({})].pack("m*")
+        field :session_id, :type => String
+        
         index :updated_at
+        index :session_id
       end
 
       # The class used for session storage.
@@ -19,33 +22,44 @@ module ActionDispatch
       SESSION_RECORD_KEY = 'rack.session.record'.freeze
 
       private
-        def generate_sid
-          BSON::ObjectID.new
-        end
-
-        def get_session(env, sid)
-          sid ||= generate_sid
+            
+        def get_session(env, sid) 
+          puts "getting session #{sid}"     
           session = find_session(sid)
           env[SESSION_RECORD_KEY] = session
           [sid, unpack(session.data)]
         end
 
         def set_session(env, sid, session_data)
-          record = env[SESSION_RECORD_KEY] ||= find_session(sid)
+          puts "setting session #{sid}"  
+          record = get_session_model(env, sid)
+          # record.data = session_data
           record.data = pack(session_data)
+          
+          # record = env[SESSION_RECORD_KEY] ||= find_session(sid)
+          # record.data = pack(session_data)
           # Rack spec dictates that set_session should return true or false
           # depending on whether or not the session was saved or not.
           # However, ActionPack seems to want a session id instead.
           record.save ? sid : false
         end
 
-        def find_session(id)        
-          @@session_class.criteria.id(id).first || @@session_class.new(:_id => id)
+        def find_session(id)   
+          puts "finding session #{id}"  
+          @@session_class.where(:session_id => id).first || @@session_class.new(:session_id => id)
         end
-        
+                
         def destroy(env)
           if sid = current_session_id(env)
             find_session(sid).destory
+          end
+        end
+        
+        def get_session_model(env, sid)
+          if env[ENV_SESSION_OPTIONS_KEY][:id].nil?
+            env[SESSION_RECORD_KEY] = find_session(sid)
+          else
+            env[SESSION_RECORD_KEY] ||= find_session(sid)
           end
         end
 
