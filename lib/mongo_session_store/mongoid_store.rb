66baa116fe3be42f89a234ac2436c02ff717b9b4
Arvid Andersson <arvid@winstondesign.se>
2011-09-20 12:46:11 +0200

    Using autoload to require the different session stores

--- lib/mongo_session_store/mongoid_store.rb
+++ lib/mongo_session_store/mongoid_store.rb
@@ -1,31 +1,27 @@
-begin
-  require 'mongoid'
-  require 'mongo_session_store/mongo_store_base'
+require 'mongoid'
+require 'mongo_session_store/mongo_store_base'
 
-  module ActionDispatch
-    module Session
-      class MongoidStore < MongoStoreBase
-        
-        class Session
-          include Mongoid::Document
-          include Mongoid::Timestamps
-          self.collection_name = MongoSessionStore.collection_name
-        
-          identity :type => String
+module ActionDispatch
+  module Session
+    class MongoidStore < MongoStoreBase
 
-          field :data, :type => BSON::Binary, :default => BSON::Binary.new(Marshal.dump({}))
-        end
+      class Session
+        include Mongoid::Document
+        include Mongoid::Timestamps
+        self.collection_name = MongoSessionStore.collection_name
 
-        private
-          def pack(data)
-            BSON::Binary.new(Marshal.dump(data))
-          end
-      
+        identity :type => String
+
+        field :data, :type => BSON::Binary, :default => BSON::Binary.new(Marshal.dump({}))
       end
+
+      private
+      def pack(data)
+        BSON::Binary.new(Marshal.dump(data))
+      end
+
     end
   end
-  
-  MongoidStore = ActionDispatch::Session::MongoidStore
-
-rescue LoadError
 end
+
+MongoidStore = ActionDispatch::Session::MongoidStore
