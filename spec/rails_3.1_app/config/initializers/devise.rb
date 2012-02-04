6a8e0be372bb3fea3bfc4beeacccd02c5212095f
Brian Hempel <bnhempel@mtu.edu>
2012-02-03 23:13:11 -0500

    Get ride of devise deprecation messages

--- spec/rails_3.1_app/config/initializers/devise.rb
+++ spec/rails_3.1_app/config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
+
+  Devise.apply_schema = false
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
--- spec/rails_3.2_app/app/models/user.rb
+++ spec/rails_3.2_app/app/models/user.rb
@@ -5,4 +5,40 @@ class User
   devise :database_authenticatable, :registerable, :validatable
          #:recoverable, :rememberable, :trackable, :validatable
 
+  # see https://github.com/plataformatec/devise/wiki/How-To:-Upgrade-to-Devise-2.0-migration-schema-style
+
+  ## Database authenticatable
+  field :email,              :type => String, :null => false
+  field :encrypted_password, :type => String, :null => false
+
+  ## Recoverable
+  # field :reset_password_token,   :type => String
+  # field :reset_password_sent_at, :type => Time
+
+  ## Rememberable
+  # field :remember_created_at, :type => Time
+
+  ## Trackable
+  # field :sign_in_count,      :type => Integer
+  # field :current_sign_in_at, :type => Time
+  # field :last_sign_in_at,    :type => Time
+  # field :current_sign_in_ip, :type => String
+  # field :last_sign_in_ip,    :type => String
+
+  ## Encryptable
+  # field :password_salt, :type => String
+
+  ## Confirmable
+  # field :confirmation_token,   :type => String
+  # field :confirmed_at,         :type => Time
+  # field :confirmation_sent_at, :type => Time
+  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
+
+  ## Lockable
+  # field :failed_attempts, :type => Integer # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
+  # field :locked_at,       :type => Time
+
+  # Token authenticatable
+  # field :authentication_token, :type => String
 end
